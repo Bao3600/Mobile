d61b72d06d07b3bc6d196c444ff3e665889d8d20
SiyuanTang1001 <46604694+SiyuanTang1001@users.noreply.github.com>
2020-11-10 19:11:16 -0500

    Update RegisterActivity.kt

--- Activity/RegisterActivity.kt
+++ Activity/RegisterActivity.kt
@@ -1,11 +1,24 @@
-package com.example.largeproject
+package com.example.foodbuddy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val moveLOGIN = findViewById<TextView>(R.id.moveLOGIN)
+// set on-click listener
+        moveLOGIN.setOnClickListener {
+            // your code to run when the user clicks on the TextView
+            val intent = Intent(this, MainActivity::class.java);
+            startActivity(intent)
+
+        }
+
+
     }
 }
