f34566dfe04741d6a2ebec3f40800b8707621ada
SiyuanTang1001 <46604694+SiyuanTang1001@users.noreply.github.com>
2020-11-10 19:10:51 -0500

    Update MainActivity.kt

--- Activity/MainActivity.kt
+++ Activity/MainActivity.kt
@@ -1,24 +1,22 @@
-package com.example.largeproject
+package com.example.foodbuddy
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TROUBLE WITH THIS
-
-        /*textView6.setOnClickListener{
+        val moveREG = findViewById<TextView>(R.id.moveREG)
+// set on-click listener
+        moveREG.setOnClickListener {
+            // your code to run when the user clicks on the TextView
             val intent = Intent(this, RegisterActivity::class.java);
             startActivity(intent)
-
-        }*/
+        }
 
     }
-
 }
