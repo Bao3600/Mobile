f789d317d28fa8acd997f469093ef6f7274ac274
Bao3600 <54757661+Bao3600@users.noreply.github.com>
2020-11-15 10:56:10 -0500

    Update MainActivity.kt
    added login functionality

--- Activity/MainActivity.kt
+++ Activity/MainActivity.kt
@@ -1,22 +1,78 @@
 package com.example.foodbuddy
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
 import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.foodbuddy.API.ApiInterface
+import com.example.foodbuddy.API.RetrofitInstance
+import com.example.foodbuddy.API.SignInBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val moveREG = findViewById<TextView>(R.id.moveREG)
-// set on-click listener
+        // set on-click listener
         moveREG.setOnClickListener {
-            // your code to run when the user clicks on the TextView
             val intent = Intent(this, RegisterActivity::class.java);
             startActivity(intent)
         }
 
+        val login = findViewById<Button>(R.id.login)
+        // set on-click listener
+        login.setOnClickListener{
+
+            val username = findViewById<EditText>(R.id.username).text.toString()
+            val password = findViewById<EditText>(R.id.password).text.toString()
+
+            //signin(username,password)
+
+            val dash = Intent(this, NavbarActivity::class.java);
+            startActivity(dash)
+        }
     }
+
+    private fun signin(username: String, password: String)
+    {
+        val retIn = RetrofitInstance.getRetrofitInstance().create(ApiInterface::class.java)
+        val signInInfo = SignInBody(username, password)
+        val dash = Intent(this, NavbarActivity::class.java);
+
+        retIn.signin(signInInfo).enqueue(object : Callback<ResponseBody>
+        {
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable)
+            {
+                Toast.makeText(
+                        this@MainActivity,
+                        t.message,
+                        Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>)
+            {
+                if (response.code() == 200)
+                {
+                    Toast.makeText(this@MainActivity, "Login success!", Toast.LENGTH_SHORT).show()
+                    startActivity(dash)
+                }
+                else
+                {
+                    Toast.makeText(this@MainActivity, "Login failed!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+    }
+
 }
