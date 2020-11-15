b7a39df2e4a18a89dbaca13ef9af04357b6146fb
Bao3600 <54757661+Bao3600@users.noreply.github.com>
2020-11-15 10:55:22 -0500

    Update RecipesFragment.kt

--- Activity/Fragment/RecipesFragment.kt
+++ Activity/Fragment/RecipesFragment.kt
@@ -1,10 +1,11 @@
-package com.example.foodbuddy
+package com.example.foodbuddy.Fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.foodbuddy.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,8 +31,8 @@ class RecipesFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_recipes, container, false)
@@ -49,11 +50,11 @@ class RecipesFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            RecipesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                RecipesFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
                 }
-            }
     }
 }
