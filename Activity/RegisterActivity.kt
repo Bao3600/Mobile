2f7046e568ca1e5e0dd657477a63b6b38d54381f
Bao3600 <54757661+Bao3600@users.noreply.github.com>
2020-11-15 10:57:56 -0500

    Update RegisterActivity.kt
    added register functionality

--- Activity/RegisterActivity.kt
+++ Activity/RegisterActivity.kt
@@ -4,6 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import android.widget.Button
+import android.widget.EditText
+import com.example.foodbuddy.API.ApiInterface
+import com.example.foodbuddy.API.RetrofitInstance
+import com.example.foodbuddy.API.UserBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +21,7 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
 
         val moveLOGIN = findViewById<TextView>(R.id.moveLOGIN)
-// set on-click listener
+        // set on-click listener
         moveLOGIN.setOnClickListener {
             // your code to run when the user clicks on the TextView
             val intent = Intent(this, MainActivity::class.java);
@@ -19,6 +29,46 @@ class RegisterActivity : AppCompatActivity() {
 
         }
 
+        val register = findViewById<Button>(R.id.register)
 
+        register.setOnClickListener {
+            val username = findViewById<EditText>(R.id.username).text.toString()
+            val fName = findViewById<EditText>(R.id.fName).text.toString()
+            val lName = findViewById<EditText>(R.id.lName).text.toString()
+            val email = findViewById<EditText>(R.id.email).text.toString()
+            val password = findViewById<EditText>(R.id.password).text.toString()
+
+            signup(username, fName, lName, email, password)
+
+        }
+
+    }
+    private fun signup(username: String, email: String, fName: String,
+                       lName: String, password: String){
+        val retIn = RetrofitInstance.getRetrofitInstance().create(ApiInterface::class.java)
+        val registerInfo = UserBody(username, email,fName, lName, password)
+        val reg = Intent(this, MainActivity::class.java)
+
+        retIn.registerUser(registerInfo).enqueue(object :
+            Callback<ResponseBody> {
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                Toast.makeText(
+                    this@RegisterActivity,
+                    t.message,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                if (response.code() == 200) {
+                    Toast.makeText(this@RegisterActivity, "Registration success!", Toast.LENGTH_SHORT)
+                        .show()
+                    startActivity(reg)
+                }
+                else{
+                    Toast.makeText(this@RegisterActivity, "Registration failed!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        })
     }
 }
