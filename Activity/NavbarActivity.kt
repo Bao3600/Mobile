4959cd856e14261459e0fd18b4440188a4362fb4
SiyuanTang1001 <46604694+SiyuanTang1001@users.noreply.github.com>
2020-11-19 14:46:20 -0500

    update to be home page

--- Activity/NavbarActivity.kt
+++ Activity/NavbarActivity.kt
@@ -1,28 +1,35 @@
 package com.example.foodbuddy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.FrameLayout
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.appcompat.app.ActionBarDrawerToggle
-import android.widget.Toast
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.FragmentTransaction
 import com.example.foodbuddy.Fragment.*
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_home.*
 
 
 class NavbarActivity : AppCompatActivity(){
 
     lateinit var toggle : ActionBarDrawerToggle
+    lateinit var editText: EditText
+    lateinit var button: Button
+    lateinit var listView: ListView
+    var list: ArrayList<String> = ArrayList()
+    lateinit var arrayAdapter: ArrayAdapter<String>
 
-    lateinit var homeFragment: HomeFragment
+    /*lateinit var homeFragment: HomeFragment
     lateinit var recipesFragment: RecipesFragment
     lateinit var addfriendFragment: AddfriendFragment
     lateinit var myaccountFragment: MyaccountFragment
     lateinit var logoutFragment: LogoutFragment
-
+*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +42,8 @@ class NavbarActivity : AppCompatActivity(){
         toggle.isDrawerIndicatorEnabled = true
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
+
+
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
@@ -49,26 +58,26 @@ class NavbarActivity : AppCompatActivity(){
             true
         }
         var navigationView: NavigationView = findViewById(R.id.navView)
-        homeFragment = HomeFragment()
-        supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.frame_layout,homeFragment)
-                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                .commit()
+        //homeFragment = HomeFragment()
+       /* supportFragmentManager
+            .beginTransaction()
+            val intent = Intent(this, NavbarActivity::class.java);
+            startActivity(intent)*/
+
+
         navigationView.setNavigationItemSelectedListener { item ->
 
             // Related fragement
             when (item.itemId) {
                 R.id.itemHome -> {
-                    homeFragment = HomeFragment()
+                    //homeFragment = HomeFragment()
                     supportFragmentManager
                             .beginTransaction()
-                            .replace(R.id.frame_layout, homeFragment)
-                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                            .commit()
+                            val intent = Intent(this, NavbarActivity::class.java);
+                            startActivity(intent)
                             drawerLayout.closeDrawers()
                 }
-                R.id.itemRecipes -> {
+                /*R.id.itemRecipes -> {
                     recipesFragment = RecipesFragment()
                     supportFragmentManager
                             .beginTransaction()
@@ -104,19 +113,50 @@ class NavbarActivity : AppCompatActivity(){
                             .commit()
                             drawerLayout.closeDrawers()
                 }
-
+*/
             }
             true
 
         }
-
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-        if (toggle.onOptionsItemSelected(item)) {
-            return true
+        listView = findViewById(R.id.listView)
+        editText = findViewById(R.id.editText)
+        button = findViewById(R.id.btnAdd)
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+        button.setOnClickListener {
+            list.add(editText.text.toString())
+            editText.setText(" ")
+            arrayAdapter.notifyDataSetChanged()
+            listView.adapter = arrayAdapter
         }
-        return super.onOptionsItemSelected(item)
-    }
+        val search = findViewById<SearchView>(R.id.searchView)
+        search .setOnQueryTextListener(object :SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                search.clearFocus()
+                if (list.contains(query))
+                {
+                    arrayAdapter.filter.filter((query))
+                }else{
+                    Toast.makeText(applicationContext,"Item not found", Toast.LENGTH_LONG).show()
+                }
+                return false
+            }
+
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                arrayAdapter.filter.filter(newText)
+                return false
+            }
+
+        })
+      
+
+}
+
+override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+ if (toggle.onOptionsItemSelected(item)) {
+     return true
+ }
+ return super.onOptionsItemSelected(item)
+}
 }
