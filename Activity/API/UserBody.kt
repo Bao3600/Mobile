4e622100fa625a5f389bee08c7c3d97fd6378fbd
Bao3600 <54757661+Bao3600@users.noreply.github.com>
2020-11-19 21:26:01 -0500

    Update UserBody.kt
    Changed to match

--- Activity/API/UserBody.kt
+++ Activity/API/UserBody.kt
@@ -1,7 +1,7 @@
 package com.example.foodbuddy.API
 
 data class UserBody(val username: String,
-                    val email: String,
                     val fName: String,
                     val lName: String,
+                    val email: String,
                     val password: String)
