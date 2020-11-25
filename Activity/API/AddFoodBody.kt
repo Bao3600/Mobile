6481644ccb96985355073937a5526b61070a87f5
Bao3600 <54757661+Bao3600@users.noreply.github.com>
2020-11-25 01:11:10 -0500

    New Add/Delete Food bodies
    New Add/Delete Food bodies

---
+++ Activity/API/AddFoodBody.kt
@@ -0,0 +1,5 @@
+package com.example.foodbuddy.API
+
+data class AddFoodBody(val item: String,
+                       val brand: String,
+                       val expDate: String)
---
+++ Activity/API/DeleteFoodBody.kt
@@ -0,0 +1,3 @@
+package com.example.foodbuddy.API
+
+data class DeleteFoodBody(val item: String)
