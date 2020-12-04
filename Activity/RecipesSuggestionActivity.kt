6445478517df7e669a1fd94c47436b4eeaf00d4d
SiyuanTang1001 <46604694+SiyuanTang1001@users.noreply.github.com>
2020-12-04 18:45:01 -0500

    Finished

--- Activity/RecipesSuggestionActivity.kt
+++ Activity/RecipesSuggestionActivity.kt
@@ -2,24 +2,36 @@ package com.example.foodbuddy
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.ListView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.navigation.NavigationView
+import com.intellif.dblib.bean.FoodDatabase
 
 
 class RecipesSuggestionActivity : AppCompatActivity() {
 
+    var list: ArrayList<String> = ArrayList()
+    lateinit var listView: ListView
     lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipes)
-        val drawerLayout =
-                findViewById<androidx.drawerlayout.widget.DrawerLayout>(R.id.drawerLayout)
-        val navView =
-                findViewById<com.google.android.material.navigation.NavigationView>(R.id.navView)
 
-        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        listView = findViewById(R.id.listView)
+
+        val drawerLayout =
+            findViewById<androidx.drawerlayout.widget.DrawerLayout>(R.id.drawerLayout)
+        val navView =
+            findViewById<com.google.android.material.navigation.NavigationView>(R.id.navView)
+
+        toggle = ActionBarDrawerToggle(
+            this, drawerLayout,
+            R.string.open,
+            R.string.close
+        )
         toggle.isDrawerIndicatorEnabled = true
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
@@ -33,7 +45,7 @@ class RecipesSuggestionActivity : AppCompatActivity() {
                 R.id.itemHome -> {
                     //homeFragment = HomeFragment()
                     supportFragmentManager
-                            .beginTransaction()
+                        .beginTransaction()
                     val intent = Intent(this, NavbarActivity::class.java);
                     startActivity(intent)
                 }
@@ -45,21 +57,46 @@ class RecipesSuggestionActivity : AppCompatActivity() {
                 R.id.itemAccount -> {
                     toMyAccount()
                 }
-                /*R.id.itemLogout -> {
-                    logoutFragment = LogoutFragment()
-                    supportFragmentManager
-                            .beginTransaction()
-                            .replace(R.id.frame_layout, LogoutFragment())
-                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                            .commit()
-                            drawerLayout.closeDrawers()
-                }
-*/
+
                 //endregion
             }
             drawerLayout.closeDrawers()
             true
 
+        }
+
+        initAdapter()
+        itemClick()
+    }
+
+    private fun initAdapter() {
+        val db = FoodDatabase.getInstance(this).getFoodDao()
+        val allFood = db.getAllFood()
+        if (allFood != null && allFood.isNotEmpty()) {
+            for (i in allFood.indices) {
+                list.add("${allFood[i].foodName}  ${allFood[i].Num}  ${allFood[i].date}")
+            }
+            var arrayAdapter = MessageAdapter(
+                this,
+                R.layout.item_nav,
+                list
+            )
+            listView.adapter = arrayAdapter
+        }
+    }
+
+    private fun itemClick(){
+        val dialog = RecipesDialog(this)
+        listView.setOnItemClickListener { adapterView, view, i, l ->
+            run {
+                val bean = list[i]
+                val split = bean.split("  ")
+                dialog.itemClickListener = {
+                    //TODO：Get Recipes 的回调
+                }
+                dialog.show()
+                Log.d("ez", "onCreate: ${split[0]} ${split[1]} ${split[2]}")
+            }
         }
     }
 
